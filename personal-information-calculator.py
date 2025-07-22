dec59595db43891cd1b3775ac502d7dce8c25260
JaiDev <jaidevyadav720@gmail.com>
2025-07-22 18:18:24 +0530

    Add personal information calculator script

---
+++ personal-information-calculator.py
@@ -0,0 +1,6 @@
+input_name = input("Enter your name: ")
+input_age = input("Enter your age: ")
+input_city = input("Enter your city: ")
+input_hobby = input("Enter your hobby: ")
+
+print(f"Hello, {input_name}!\nYou are {input_age} years old and live in {input_city}.\nIn your free time , you enjoy {input_hobby}.")
